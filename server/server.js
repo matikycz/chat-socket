afbb8e36da99077a1b1ab58464853b515afbe5bf
Mateusz Sitarczyk <mateo@localhost.localdomain>
2016-09-19 23:55:38 +0200

    socket io test

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+const server = app.listen(3000, () => {
+  console.log('server started on port 3000')
+})
+
+const io = require('socket.io').listen(server)
+
+app.use(cors({credentials: true, origin: true}))
 
 app.get('/', (req, res) => {
   res.send('Hello world!')
 })
 
-const server = app.listen(3000, () => {
-  console.log('server started on port 3000')
+io.on('connection', socket => {
+  console.log('user connected')
+  socket.on('disconnect', () => {
+    console.log('disconnected')
+  })
+  socket.on('room', room => {
+    console.log(`joined room ${room}`)
+  })
 })
